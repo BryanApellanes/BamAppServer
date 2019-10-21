6696d34feb2320c104aa1189f0d76420b388a90d
Bryan Apellanes <bapellanes@vimly.com>
2019-10-21 08:50:49 -0700

    commit admin folder

---
+++ apps/admin/pages/Vimly/main.js
@@ -0,0 +1,15 @@
+// Main
+
+window.handlebars = require('handlebars');
+
+window.qi = require('../../js/bam/data/qi.js');
+window.sdo = require('../../js/bam/data/sdo.js');
+window.dao = require('../../js/bam/data/dao.js');
+window.bam = require('../../js/bam/bam.js')({});
+window.obj = require('../../js/bam/data/objToArray');
+
+window.vimly = require('./vimly.js');
+window.environments = require('./environments.js');
+window.effectiveDate = require('./effectiveDate.js');
+
+require('./PlanDocuments/PlanDocuments.js');
