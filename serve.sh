9b07184d099396e7f258da16134bbac234d562be
Bryan Apellanes <bapellanes@vimly.com>
2019-10-21 11:48:26 -0700

    serve script

---
+++ serve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $1 = "-help" ]] || [[ $1 = "-?" ]] || [[ $1 = "-h" ]]
+then
+    printf "usage: serve.sh\r\n"
+    printf "\r\n"
+    printf "serve the default section of the default application"
+    printf "\r\n"
+
+else
+
+APPNAME=$(<defaultappname)
+SECTION=$(<apps/$APPNAME/defaultsection)
+
+echo serving $SECTION section of $APPNAME
+
+cd apps/$APPNAME
+./serve-section.sh $SECTION
+
+fi
