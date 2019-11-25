278376a63ccdc04e2f85312e2753beeea76cc964
Bryan Apellanes <bapellanes@vimly.com>
2019-11-25 10:33:52 -0800

    perms and docker scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,20 @@
-APPNAME=$(<defaultappname)
+#!/bin/bash
 
-docker build -t $APPNAME .
+if [[ -z "$1" ]] || [[ $1 = "-help" ]] || [[ $1 = "-?" ]] || [[ $1 = "-h" ]]; then
+    printf "usage: docker-build .sh <image-tag> [Dockerfile]\r\n"
+    printf "\r\n"
+    printf "Using docker, creates a container image using the specified tag/name and optional dockerfile.\r\n"
+    printf "\r\n"
+    exit 0
+else
+
+TAG=$1
+FILE=Dockerfile
+
+if [[ $# -eq 2 ]]; then
+    FILE=$2
+fi
+
+docker build -t $TAG . -f $FILE
+
+fi
