cf04e6516fce11b89cc20b2411ab441a53fd8915
Bryan Apellanes <bapellanes@vimly.com>
2019-12-17 10:29:10 -0800

    aws cli install script updated

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -10,4 +10,4 @@ fi
 
 curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-./aws/install
+./aws/install -i ~/.local/aws-cli -b ~/.local/bin
