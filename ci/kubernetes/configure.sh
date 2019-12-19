6b1264d522ca5f9c253e19f315e9966da95ea131
Bryan Apellanes <bapellanes@vimly.com>
2019-12-19 14:26:13 -0800

    kubectl configure.sh

--- ci/kubernetes/configure.sh
+++ ci/kubernetes/configure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
 
 echo "*** Configuring kubectl"
-kubectl config set-cluster simon365-test --server=$KUBERNETES_SERVER --insecure-skip-tls-verify=true
-kubectl config set-credentials simon365-test --token=$KUBERNETES_TOKEN
+echo "*** Server is ${KUBERNETES_SERVER}"
+kubectl config set-cluster simon365-test --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+kubectl config set-credentials simon365-test --token=${KUBERNETES_TOKEN}
 kubectl config set-context bitbucket-pipeline-test --cluster=simon365-dev --user=simon365-test
 
 kubectl config use-context bitbucket-pipeline-test
