4452e8bb3b646c46ece0dcf3c22c0ef715ed5f84
Bryan Apellanes <bapellanes@vimly.com>
2020-01-03 10:10:59 -0800

    move shell script

--- ci/kubernetes/add-app-host-binding.sh
+++ ci/kubernetes/add-app-host-binding.sh
@@ -3,7 +3,7 @@
 HOSTNAME=`kubectl get services -n vimly-admin -o json | jq '.items[0].status.loadBalancer.ingress[0].hostname'`
 # trim the quotes
 HOSTNAME=`echo $HOSTNAME | cut -c 2-`
-HOSTNAME=${HOSTNAME%?}
+export HOSTNAME=${HOSTNAME%?}
 # / trim the quotes
 
-node ${APPROOT}/common/bamjs/bam.js cli -app:$APPNAME -add:app-host-binding -hostName:$HOSTNAME -content:${APPROOT}
+node ./node/cli.js shellExecute -app:${APPNAME} -add:app-host-binding -hostName:${HOSTNAME} -content:${APPROOT}
